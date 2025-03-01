b98702454d4812d205a7eb64a9269811d604fcea
AlexandreBottet <alexandre.bottet@outlook.fr>
2025-03-01 14:05:16 +0100

    feat: Implement Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn
+    attr_reader :string, :card
+
+    def initialize(string, card)
+        @string = string
+        @card = card
+    end
+
+    def guess
+        @string
+    end
+
+    def correct?
+        @string == @card.answer
+    end
+
+    def feedback
+        if @string == @card.answer
+            'Correct!'
+        else
+            'Incorrect.'
+        end
+    end
+end
